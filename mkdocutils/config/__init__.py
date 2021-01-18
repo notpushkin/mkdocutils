4cf3e1a99de788c76fe04e33c4267344d807dd58
Alexander Pushkov <alexander@notpushk.in>
2021-01-18 03:42:28 +0300

    Clone mkdocs

---
+++ mkdocutils/config/__init__.py
@@ -0,0 +1,8 @@
+from mkdocutils.config.base import Config, load_config
+from mkdocutils.config.defaults import DEFAULT_SCHEMA
+
+__all__ = [
+    load_config.__name__,
+    Config.__name__,
+    'DEFAULT_SCHEMA',
+]
